fb2f1dd2204cd56b155eee69495339bd61e54937
tomhosker <tomdothosker@gmail.com>
2021-10-30 21:39:00 +0100

    Moved slideshow script to its own file.

---
+++ views/frontend_scripts/slideshow.js
@@ -0,0 +1,18 @@
+/*
+This is a front end script which defines a class modelling a slideshow.
+*/
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) slideIndex = 1;
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
