9d7406c38d62892aec7c6dedcd948d2e5545f168
tomhosker <tomdothosker@gmail.com>
2022-06-05 18:40:37 +0100

    Adding changing colours...

--- public/frontend_scripts/liturgical_colours.js
+++ public/frontend_scripts/liturgical_colours.js
@@ -13,7 +13,7 @@ function recolorElements(className, field, color) {
 // The class in question.
 class ColorChanger {
     constructor() {
-        this.litColor = "deeppink";
+        this.litColor = "gold";
     }
 
     recolorElements(className, field) {
