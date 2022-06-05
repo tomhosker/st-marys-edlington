d3731bef398d5d0699cf2f358f07b5774b95adf0
tomhosker <tomdothosker@gmail.com>
2022-06-05 15:39:17 +0100

    Adding changing colours...

---
+++ public/frontend_scripts/liturgical_colours.js
@@ -0,0 +1,22 @@
+/*
+This is a front end script which changes the colours of elements according to
+the current liturgical colour.
+*/
+
+// The class in question.
+class ColourChanger {
+    constructor() {
+        this.helpboxes = document.getElementsByClassName("helpbox");
+    }
+
+    // Change the colour of each element.
+    changeColours() {
+        for (let i = 0; i < this.helpboxes.length; i++) {
+            this.helpboxes[i].class = "helpbox.green";
+        }
+    }
+}
+
+// Let's get cracking...
+const colourChanger = new ColourChanger();
+colourChanger.changeColours();
