43473fb69f8ee722379bea8f3cb2dd160e8a16a2
tomhosker <tomdothosker@gmail.com>
2022-06-05 20:52:58 +0100

    Adding changing colours...

--- public/frontend_scripts/browserified/liturgical_colours.js
+++ public/frontend_scripts/browserified/liturgical_colours.js
@@ -43171,7 +43171,7 @@ class ColorChanger {
         const calendar = RomCal.calendarFor();
         const today = new Date();
         const todayAtMidnight =
-            new Date(today.getFullYear(), today.getMonth(), today.getDate());
+            new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0);
         const moment = todayAtMidnight.toISOString();
         let result;
 
