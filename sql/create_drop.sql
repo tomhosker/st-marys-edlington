9ff4c9ef9330c402762117684dbf0d4358abe14c
tomhosker <tomdothosker@gmail.com>
2023-04-24 00:00:53 +0100

    Debugging...

--- sql/create_drop.sql
+++ sql/create_drop.sql
@@ -35,9 +35,7 @@ CREATE TABLE Contact (
     mobile VARCHAR(99),
     email VARCHAR(99),
     address VARCHAR(99),
-    CONSTRAINT fk_address
-        FOREIGN KEY(address)
-            REFERENCES RealWorldAddress(code)
+    CONSTRAINT fk_address FOREIGN KEY(address) REFERENCES RealWorldAddress(code)
 );
 
 CREATE TABLE ServiceTime (
@@ -53,18 +51,15 @@ CREATE TABLE ServiceTime (
     has_childrens_liturgy BOOLEAN NOT NULL DEFAULT false,
     childrens_liturgy_remarks VARCHAR(999),
     remarks VARCHAR(99),
-    CONSTRAINT fk_location
-        FOREIGN KEY(location)
-            REFERENCES RealWorldAddress(code)
+    CONSTRAINT fk_location FOREIGN KEY(location) REFERENCES
+        RealWorldAddress(code)
 );
 
 CREATE TABLE ParishRole (
     code VARCHAR(99) PRIMARY KEY,
     description VARCHAR(99) NOT NULL DEFAULT 'INSERT ROLE DESCRIPTION HERE',
     contact VARCHAR(99) NOT NULL DEFAULT 'father-john',
-    CONSTRAINT fk_contact
-        FOREIGN KEY(contact)
-            REFERENCES Contact(code)
+    CONSTRAINT fk_contact FOREIGN KEY(contact) REFERENCES Contact(code)
 );
 
 CREATE TABLE Newsletter (
@@ -72,7 +67,8 @@ CREATE TABLE Newsletter (
     week_beginning_day INT NOT NULL DEFAULT 1, -- Day of the month.
     week_beginning_month INT NOT NULL DEFAULT 0, -- 0 = January, 11 = December.
     week_beginning_year INT NOT NULL DEFAULT 2000,
-    link VARCHAR(99) NOT NULL DEFAULT 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    link VARCHAR(99) NOT NULL DEFAULT
+        'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 );
 
 CREATE TABLE Pilgrimage (
@@ -92,5 +88,6 @@ CREATE TABLE OtherDocument (
     id SERIAL PRIMARY KEY,
     name VARCHAR(99),
     publication_date DATE DEFAULT CURRENT_DATE,
-    link VARCHAR(99) NOT NULL DEFAULT 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    link VARCHAR(99) NOT NULL DEFAULT
+        'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 );
